705258e7dbce25ee4e1b08fa85781e7e595c98f9
miltonmurray <miltonmurray@github.com>
2019-07-04 16:21:24 -0400

    Fixes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-<<<<<<< HEAD
 import SeasonDisplay from './SeasonDisplay';
 import Spinner from './Spinner';
 
@@ -26,20 +25,8 @@ class App extends React.Component{
     }
 
     render(){ 
-        return <div className='border red'>{this.renderContent()}</div>
+        return <div className='container'>{this.renderContent()}</div>
     }
 }
 
 ReactDOM.render(<App />, document.querySelector('#root'));
-=======
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
->>>>>>> 234555524b6d8d184a460d358d9f63b7a5e35b5b
